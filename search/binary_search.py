9d01f223427d2e53a3bc5da9ddfab075a0e27caa
Kelvin Li <seawaywen@gmail.com>
2018-10-24 09:04:50 +0800

    Update the incursive binary_search implementation to fix the bug and ad the additonal data list type checking

--- search/binary_search.py
+++ search/binary_search.py
@@ -2,26 +2,46 @@
 # -*- coding: utf-8 -*-
 
 def binary_search_recursive(data_list, key):
-    """Recursive binary search for the given sorted integer list
-
+    """Recursive binary search for the given sorted integer list.
+    Simple call the recursive helper
     Args:
         data_list: sorted integer list
         key: the integer to be searched
     Returns:
         the first found key's index in the list
     """
-    length = len(data_list)
-    if length == 0:
+    if not isinstance(data_list, (list, tuple)):
+        print('invalid data list')
         return -1
 
-    mid = length // 2
-    if key == data_list[mid]:
-        return mid
+    return binary_search_recursive_helper(
+        data_list, key, 0, len(data_list) - 1)
 
-    if key < data_list[mid]:
-        return binary_search_recursive(data_list[:mid], key)
+def binary_search_recursive_helper(data_list, key, low, high):
+    """Recursive binary search helper for the given sorted integer list
+
+    Args:
+        data_list: sorted integer list
+        key: the integer to be searched
+        low: list lowest index
+        high: list highest index
+    Returns:
+        the first found key's index in the list
+    """
+    if low > high:
+        return -1
+
+    mid = low + ((high - low) >> 1)
+    if data_list[mid] == key:
+        return mid
     else:
-        return mid + 1 + binary_search_recursive(data_list[mid+1:], key)
+        if data_list[mid] < key:
+            return binary_search_recursive_helper(
+                data_list, key, mid + 1, high)
+        else:
+            return binary_search_recursive_helper(
+                data_list, key, low, mid - 1)
+
 
 
 def binary_search_iterative(data_list, key):
@@ -33,6 +53,10 @@ def binary_search_iterative(data_list, key):
     Returns:
         the first found key's index in the list
     """
+    if not isinstance(data_list, (list, tuple)):
+        print('invalid data list')
+        return -1
+
     length = len(data_list)
     if length == 0:
         return -1
@@ -40,7 +64,12 @@ def binary_search_iterative(data_list, key):
     low = 0
     high = length - 1
     while low <= high:
-        mid = (low + high) // 2
+        # following calc may cause the overflow if number is big enough
+        #mid = (low + high) // 2
+        # alternitive 1:
+        #mid = low + (high - low) // 2
+        # alternitive 2:
+        mid = low + ((high - low) >> 1)
 
         if data_list[mid] == key:
             return mid
@@ -65,7 +94,15 @@ if __name__ == '__main__':
     print(binary_search_recursive(data, 7))
     print(binary_search_iterative(data, 7))
 
-    #case3: non-existed element
+    #case3: non-existed element(low bound number)
     print(binary_search_recursive(data, -100))
     print(binary_search_iterative(data, -100))
+
+    #case4: non-existed element(high bound number)
+    print(binary_search_iterative(data, 1001))
+    print(binary_search_recursive(data, 1001))
+
+    #case5: invalid data list
+    print(binary_search_iterative(12, 1001))
+    print(binary_search_recursive(12, 1001))
 
